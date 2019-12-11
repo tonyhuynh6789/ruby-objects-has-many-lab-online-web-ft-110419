f16366dff3a3135051c614a665fdd9ce6e3d8140
Tony Rim <duc_thinh_03@yahoo.com>
2019-12-11 22:46:09 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,6 +6,10 @@ attr_accessor :name
 
 def initialize(name)
     @name = name 
+<<<<<<< HEAD
+=======
+    @@song_count += 1
+>>>>>>> 34c39cc60ae9f2a572739bf5aad41f7ed2416493
 end 
 
 
@@ -24,7 +28,10 @@ def add_song_by_name(name)
     song.artist = self
 end  
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> 34c39cc60ae9f2a572739bf5aad41f7ed2416493
 def self.song_count 
     Song.all.size
 end 
